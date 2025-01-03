3d86f4777bd4ed6d1eb46034097190a84af72568
Maxylan <maxylan@github.com>
2025-01-03 15:24:04 +0100

    Mage - Init commit

---
+++ Storage/init/seed.sql
@@ -0,0 +1,16 @@
+-- Truncate tables (in case they already existed..)
+TRUNCATE TABLE accounts CASCADE;
+TRUNCATE TABLE sessions CASCADE;
+TRUNCATE TABLE categories CASCADE;
+TRUNCATE TABLE albums CASCADE;
+TRUNCATE TABLE tags CASCADE;
+TRUNCATE TABLE photos CASCADE;
+TRUNCATE TABLE filepaths CASCADE;
+TRUNCATE TABLE photo_tags CASCADE;
+TRUNCATE TABLE photo_albums CASCADE;
+TRUNCATE TABLE album_tags CASCADE;
+TRUNCATE TABLE logs CASCADE;
+
+INSERT INTO accounts (email, username, password, full_name, permissions) VALUES(
+    'webmaster@torpssons.se', 'root', 'password', 'Administrator', 15
+);
