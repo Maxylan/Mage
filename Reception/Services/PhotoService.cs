18d787da037452524e503e4279ad65cfed0737ae
Maxylan <maxylan@github.com>
2025-01-21 18:56:32 +0100

    Implemented methods of IPhotoService

--- Reception/Services/PhotoService.cs
+++ Reception/Services/PhotoService.cs
@@ -14,34 +14,52 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Get the <see cref="PhotoEntity"/> (<seealso cref="Reception.Models.Entities.Photo"/>) with Primary Key '<paramref ref="photoId"/>'
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> GetPhotoEntity(int photoId);
+    public async Task<ActionResult<PhotoEntity>> GetPhotoEntity(int photoId)
+    {
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// Get the <see cref="PhotoEntity"/> (<seealso cref="Reception.Models.Entities.Photo"/>) with Slug '<paramref ref="slug"/>' (string)
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> GetPhotoEntity(string slug);
+    public async Task<ActionResult<PhotoEntity>> GetPhotoEntity(string slug)
+    {
+        throw new NotImplementedException();
+    }
 
 
     /// <summary>
     /// Get the <see cref="Reception.Models.Photo"/> with Primary Key '<paramref ref="photoId"/>'
     /// </summary>
-    public abstract Task<ActionResult<Photo>> GetSinglePhoto(int photoId);
+    public async Task<ActionResult<Photo>> GetSinglePhoto(int photoId)
+    {
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// Get the <see cref="Reception.Models.Photo"/> with Slug '<paramref ref="slug"/>' (string)
     /// </summary>
-    public abstract Task<ActionResult<Photo>> GetSinglePhoto(string slug);
+    public async Task<ActionResult<Photo>> GetSinglePhoto(string slug)
+    {
+        throw new NotImplementedException();
+    }
 
 
     /// <summary>
     /// Get the <see cref="PhotoCollection"/> with Primary Key '<paramref ref="photoId"/>'
     /// </summary>
-    public abstract Task<ActionResult<PhotoCollection>> GetPhoto(int photoId);
+    public async Task<ActionResult<PhotoCollection>> GetPhoto(int photoId)
+    {
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// Get the <see cref="PhotoCollection"/> with Slug '<paramref ref="slug"/>' (string)
     /// </summary>
-    public abstract Task<ActionResult<PhotoCollection>> GetPhoto(string slug);
+    public async Task<ActionResult<PhotoCollection>> GetPhoto(string slug)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -49,7 +67,7 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Get all <see cref="Reception.Models.Photo"/> instances matching a wide range of optional filtering / pagination options (<seealso cref="FilterPhotosOptions"/>).
     /// </summary>
-    public virtual Task<ActionResult<IEnumerable<Photo>>> GetSingles(Action<FilterPhotosOptions> opts)
+    public Task<ActionResult<IEnumerable<Photo>>> GetSingles(Action<FilterPhotosOptions> opts)
     {
         FilterPhotosOptions filtering = new();
         opts(filtering);
@@ -60,14 +78,17 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Get all <see cref="Reception.Models.Photo"/> instances matching a wide range of optional filtering / pagination options (<seealso cref="FilterPhotosOptions"/>).
     /// </summary>
-    public abstract Task<ActionResult<IEnumerable<Photo>>> GetSingles(FilterPhotosOptions filter);
+    public async Task<ActionResult<IEnumerable<Photo>>> GetSingles(FilterPhotosOptions filter)
+    {
+        throw new NotImplementedException();
+    }
 
 
     /// <summary>
     /// Assemble an <see cref="IEnumerable{Reception.Models.PhotoCollection}"/> collection of Photos matching a wide range of optional 
     /// filtering / pagination options (<seealso cref="FilterPhotosOptions"/>).
     /// </summary>
-    public virtual Task<ActionResult<IEnumerable<PhotoCollection>>> GetPhotos(Action<FilterPhotosOptions> opts)
+    public Task<ActionResult<IEnumerable<PhotoCollection>>> GetPhotos(Action<FilterPhotosOptions> opts)
     {
         FilterPhotosOptions filtering = new();
         opts(filtering);
@@ -79,7 +100,10 @@ public class PhotoService : IPhotoService
     /// Assemble an <see cref="IEnumerable{Reception.Models.PhotoCollection}"/> collection of Photos matching a wide range of optional 
     /// filtering / pagination options (<seealso cref="FilterPhotosOptions"/>).
     /// </summary>
-    public abstract Task<ActionResult<IEnumerable<PhotoCollection>>> GetPhotos(FilterPhotosOptions filter);
+    public async Task<ActionResult<IEnumerable<PhotoCollection>>> GetPhotos(FilterPhotosOptions filter)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -92,7 +116,7 @@ public class PhotoService : IPhotoService
     /// <see cref="Reception.Models.Entities.Photo"/> database entity.
     /// </remarks>
     /// <returns><see cref="PhotoCollection"/></returns>
-    public virtual Task<ActionResult<PhotoCollection>> CreatePhoto(Action<FilterPhotosOptions> opts)
+    public Task<ActionResult<PhotoCollection>> CreatePhoto(Action<FilterPhotosOptions> opts)
     {
         FilterPhotosOptions filtering = new();
         opts(filtering);
@@ -108,7 +132,10 @@ public class PhotoService : IPhotoService
     /// <see cref="Reception.Models.Entities.Photo"/> database entity.
     /// </remarks>
     /// <returns><see cref="PhotoCollection"/></returns>
-    public abstract Task<ActionResult<PhotoCollection>> CreatePhoto(FilterPhotosOptions details);
+    public async Task<ActionResult<PhotoCollection>> CreatePhoto(FilterPhotosOptions details)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -116,13 +143,16 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Create a <see cref="Reception.Models.Entities.Photo"/> in the database.
     /// </summary>
-    public virtual Task<ActionResult<PhotoEntity>> CreatePhotoEntity(MutatePhoto mut) =>
+    public Task<ActionResult<PhotoEntity>> CreatePhotoEntity(MutatePhoto mut) =>
         CreatePhotoEntity((PhotoEntity)mut);
 
     /// <summary>
     /// Create a <see cref="Reception.Models.Entities.Photo"/> in the database.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> CreatePhotoEntity(PhotoEntity entity);
+    public async Task<ActionResult<PhotoEntity>> CreatePhotoEntity(PhotoEntity entity)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -130,18 +160,24 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Updates a <see cref="Reception.Models.Entities.Photo"/> (..identified by PK <paramref name="photoId"/>) ..in the database.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(int photoId);
+    public async Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(int photoId)
+    {
+        throw new NotImplementedException();
+    }
 
     /// <summary>
     /// Updates a <see cref="Reception.Models.Entities.Photo"/> in the database.
     /// </summary>
-    public virtual Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(MutatePhoto mut) =>
+    public Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(MutatePhoto mut) =>
         UpdatePhotoEntity((PhotoEntity)mut);
 
     /// <summary>
     /// Updates a <see cref="Reception.Models.Entities.Photo"/> in the database.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(PhotoEntity entity);
+    public async Task<ActionResult<PhotoEntity>> UpdatePhotoEntity(PhotoEntity entity)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -150,12 +186,18 @@ public class PhotoService : IPhotoService
     /// Deletes a <see cref="Reception.Models.Entities.Photo"/> (..identified by PK <paramref name="photoId"/>) ..completely,
     /// removing both the blob on-disk, and its database entry.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> DeletePhoto(int photoId);
+    public async Task<ActionResult<PhotoEntity>> DeletePhoto(int photoId)
+    {
+        throw new NotImplementedException();
+    }
     /// <summary>
     /// Deletes a <see cref="Reception.Models.Entities.Photo"/> (..identified by PK <paramref name="photoId"/>) ..completely,
     /// removing both the blob on-disk, and its database entry.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> DeletePhoto(PhotoEntity entity);
+    public async Task<ActionResult<PhotoEntity>> DeletePhoto(PhotoEntity entity)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -163,7 +205,10 @@ public class PhotoService : IPhotoService
     /// <summary>
     /// Deletes the blob of a <see cref="Reception.Models.Entities.Photo"/> from disk.
     /// </summary>
-    public abstract Task<ActionResult<PhotoEntity>> DeletePhotoBlob(Filepath entity);
+    public async Task<ActionResult<PhotoEntity>> DeletePhotoBlob(Filepath entity)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 
 
@@ -174,7 +219,7 @@ public class PhotoService : IPhotoService
     /// <remarks>
     /// <strong>Note:</strong> Since this does *not* delete the blob on-disk, be mindful you don't leave anything dangling..
     /// </remarks>
-    public virtual Task<ActionResult<PhotoEntity>> DeletePhotoEntity(MutatePhoto mut) =>
+    public Task<ActionResult<PhotoEntity>> DeletePhotoEntity(MutatePhoto mut) =>
         UpdatePhotoEntity((PhotoEntity)mut);
 
     /// <summary>
@@ -183,6 +228,9 @@ public class PhotoService : IPhotoService
     /// <remarks>
     /// <strong>Note:</strong> Since this does *not* delete the blob on-disk, be mindful you don't leave anything dangling..
     /// </remarks>
-    public abstract Task<ActionResult<PhotoEntity>> DeletePhotoEntity(PhotoEntity entity);
+    public async Task<ActionResult<PhotoEntity>> DeletePhotoEntity(PhotoEntity entity)
+    {
+        throw new NotImplementedException();
+    }
     #endregion
 }
