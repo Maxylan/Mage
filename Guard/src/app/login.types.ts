b61d983c322cbca72209b0ae7018f1e7bb7db9a1
Maxylan <maxylan@github.com>
2025-02-15 19:55:18 +0100

    Fixed routing issues in 'Guard/mage.conf' and added new favicon ofr Guard

--- Guard/src/app/login.types.ts
+++ Guard/src/app/login.types.ts
@@ -2,3 +2,17 @@ export interface HashedUserDetails {
     username: string,
     password: string
 }
+
+export type LoginBody = {
+    username: string,
+    password: string
+}
+
+export type Session = {
+    id: number;
+    userId: number;
+    code: string;
+    userAgent?: string|null;
+    createdAt: string;
+    expiresAt: string;
+}
