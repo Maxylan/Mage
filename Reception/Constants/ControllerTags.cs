4f92c5dbde35314af5bbc1dd54649b9a2b68dde8
Maxylan <maxylan@torpssons.se>
2025-02-27 21:06:43 +0100

    Big updates to the PhotosController

---
+++ Reception/Constants/ControllerTags.cs
@@ -0,0 +1,10 @@
+namespace Reception.Constants;
+
+/// <summary>
+/// Controller Constants, tags for endpoints to help separate them in Swagger UI.
+/// </summary>
+public static class ControllerTags
+{
+    public const string PHOTOS_ENTITIES = "Photos";
+    public const string PHOTOS_FILES = "Files";
+}
